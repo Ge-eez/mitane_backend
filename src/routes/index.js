0eace1ac1ec2f6857d2d08f8e1996a65542b8ff3
Elshadai <elshadaikassutegegn@gmail.com>
2021-08-04 18:09:29 +0300

    Views deleted to use json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/**
+ * Returns API status
+ * 
+ * @route GET /
+ * @group index - Validates and gives back API service status
+ * @returns {object} 200 - {
+ *  title: 'Mitane api',
+ *  version: '1.0.0',
+ *   description: 'description .....'
+ * }
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/', async function(req, res, next) {
+  return res.json({
+    title: 'Mitane api',
+    version: '1.0.0',
+    description: 'description .....'
+  })
 });
 
 module.exports = router;
