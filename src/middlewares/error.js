966986732ec000d299d177fea8c6805d5a369e61
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-05 20:54:58 +0300

    bug fixed

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,8 +2,7 @@ const ErrorResponse = require('../utility/errorResponse');
 
 
 const errorHandler = (err,req,res,next)=>{
-    console.log(err.name);
-    res.status(err.status|500).json({
+    res.status(err.statusCode || 500).json({
         success:false,
         data: err.message
     });
