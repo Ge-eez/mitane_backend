6c48c1b83194edbca0d3fab9de09c456c51b63d9
Eden Alem <edenberhanalem@gmail.com>
2021-08-11 21:45:48 -0400

    Bug fix

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,6 +15,9 @@ router.get('/id/:id', userController.getUserById);
 
 //to be worked on
 router.get('/role/:role', userController.getUserByRole);
+router.get('/create', userController.createUser);
+router.get('/update/:id', userController.updateUser);
+router.get('/delete/:id', userController.deleteUser);
 
 module.exports = router;
 
