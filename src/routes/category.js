1a56aec763d24d43c97e96ce4dad5503b01ffd4f
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-14 22:04:19 +0300

    category route modified

--- src/routes/category.js
+++ src/routes/category.js
@@ -5,6 +5,7 @@ const {
     updateCategory,
     deleteCategory
 } = require('../controllers/category-controller');
+const {permission} = require('../middlewares/permission');
 
 //Include other resource routes
 const productRouter = require('./product');
@@ -13,16 +14,68 @@ const express = require('express');
 const router = express.Router();
 
 //Re-route to other resource route
-router.use('/:categoryId/products',productRouter);
+router.use('/:categoryName/products',productRouter);
 
-router.route('/')
-    .get(getCategory)
-    .post(createCategory);
+/**
+ * Get category 
+ * 
+ * @route GET /category
+ * @group Category 
+ * @security JWT
+ * @param {string} keyword.query - category name
+ * @returns {object} 200 - Category object
+ * @returns {Error}  default - Unexpected error
+ */
+router.route('/').get(getCategory);
 
-router.route('/:id')
-    .get(getCategoryById)
-    .put(updateCategory)
-    .delete(deleteCategory);
+/**
+ * Create category 
+ * 
+ * @route Post /category
+ * @group Category 
+ * @security JWT
+ * @param {string} name.body.required - Category name
+ * @returns {object} 200 - A category object
+ * @returns {Error}  default - Unexpected error
+ */
+router.route('/').post(permission(),createCategory);
+
+/**
+ * Get a category by Id
+ * 
+ * @route GET /category/{id}
+ * @group Category 
+ * @security JWT
+ * @param {string} id.path.required - category id
+ * @returns {object} 200 - A single category object
+ * @returns {Error}  default - Unexpected error
+ */
+router.route('/:id').get(getCategoryById);
+
+/**
+ * Update a category 
+ * 
+ * @route Put /category/{name}
+ * @group Category 
+ * @security JWT
+ * @param {string} - Category name
+ * @param {string} name.path.required - category name
+ * @returns {object} 200 - Category Object
+ * @returns {Error}  default - Unexpected error
+ */
+router.route('/:name').put(permission(),updateCategory);
+
+/**
+ * Delete a category 
+ * 
+ * @route Delete /category/{name}
+ * @group Category 
+ * @security JWT
+ * @param {string} name.path.required - category name
+ * @returns {object} 200 - Category Object
+ * @returns {Error}  default - Unexpected error
+ */
+router.route('/:name').delete(permission(),deleteCategory);
 
 
 module.exports = router;
