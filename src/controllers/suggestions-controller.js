a92447fcdd56e56666e5f2a8e08763dd5f8d5c03
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2021-09-07 09:53:05 +0300

    suggestions page added

--- src/controllers/suggestions-controller.js
+++ src/controllers/suggestions-controller.js
@@ -1,40 +1,40 @@
 const asyncHandler = require('../middlewares/async');
 const ErrorResponse = require('../utility/errorResponse');
-const Products = require('../models/product-model');
-const Price = require('../models/price-model');
-exports.getSuggestionsBasedOnPrice = asyncHandler(async(req,res,next) =>
-{   
-    let price = {price_of_the_day:{price}}
-    let suggestions = await Price.aggregate([
-        { $lookup:
-           {
-             from: 'Products',
-             localField: 'product',
-             foreignField: 'name',
-             as: 'suggestedProducts'
-           },
-         },
+// const Products = require('../models/product-model');
+// const Price = require('../models/price-model');
+// exports.getSuggestionsBasedOnPrice = asyncHandler(async(req,res,next) =>
+// {   
+//     let price = {price_of_the_day:{price}}
+//     let suggestions = await Price.aggregate([
+//         { $lookup:
+//            {
+//              from: 'Products',
+//              localField: 'product',
+//              foreignField: 'name',
+//              as: 'suggestedProducts'
+//            },
+//          },
 
-         {
-             $unwind: "$suggestedProducts"
-         },
-         {
-            $sort:  {price :-1}
-         },
-         {$limit:10}
-        ])
+//          {
+//              $unwind: "$suggestedProducts"
+//          },
+//          {
+//             $sort:  {price :-1}
+//          },
+//          {$limit:10}
+//         ])
 
-    res.status(200).json({
-        status:true,
-        data: suggestions
-    });
-
-
-}
-)
+//     res.status(200).json({
+//         status:true,
+//         data: suggestions
+//     });
 
 
-exports.getSuggestionsBasedOnWeather
+// }
+// )
+
+
+// exports.getSuggestionsBasedOnWeather
 
 
 
