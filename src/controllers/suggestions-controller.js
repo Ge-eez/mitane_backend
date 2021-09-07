34181cf2638a044aa2e93afef6fb4920d482c4ba
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2021-09-07 11:30:31 +0300

    suggestion function added

--- src/controllers/suggestions-controller.js
+++ src/controllers/suggestions-controller.js
@@ -3,17 +3,17 @@ const ErrorResponse = require('../utility/errorResponse');
 const Products = require('../models/product-model');
 const Price = require('../models/price-model');
 exports.getSuggestionsBasedOnPrice = asyncHandler(async(req,res,next) =>
-// {   
-//     let price = {price_of_the_day:{price}}
-//     let suggestions = await Price.aggregate([
-//         { $lookup:
-//            {
-//              from: 'Products',
-//              localField: 'product',
-//              foreignField: 'name',
-//              as: 'suggestedProducts'
-//            },
-//          },
+{   
+    let price = {price_of_the_day:{price}}
+    let suggestions = await Price.aggregate([
+        { $lookup:
+           {
+             from: 'Products',
+             localField: 'product',
+             foreignField: 'name',
+             as: 'suggestedProducts'
+           },
+         },
 
 //          {
 //              $unwind: "$suggestedProducts"
